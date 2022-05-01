93190c230dd39407fb63c6e7cdc397e676c050a5
albasimia <albasimia888@gmail.com>
2022-05-02 00:59:22 +0900

    第2章まで。

---
+++ src/js/speed0.js
@@ -0,0 +1,23 @@
+var ctx, posX = 0,
+    velocityX = 5;
+
+function init() {
+    ctx = document.getElementById("viewer").getContext("2d");
+    setInterval(tick, 100);
+}
+
+function tick() {
+    posX += velocityX;
+    if (posX > 600) posX = 0;
+    paint();
+}
+
+function paint() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 600, 600);
+    ctx.fillStyle = "white";
+    ctx.fillRect(posX, 200, 10, 10);
+    ctx.fill();
+}
+
+document.querySelector('body').addEventListener('load', init())
